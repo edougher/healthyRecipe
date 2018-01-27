36794b56120c2e92910c686b8b043a381e9ae90d
Erin Dougher <edougher@students.mchenry.edu>
2018-01-27 17:42:18 -0600

    Initial commit

---
+++ app/src/main/java/com/example/aaron/healthyrecipe/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.aaron.healthyrecipe;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = (Button)findViewById(R.id.btnRecpie);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Recipe.class));
+            }
+        });
+
+
+    }
+
+
+}
